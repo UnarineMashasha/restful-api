0c215a63ee29790af4e44d71dc367ee3ba26707b
UnarineMashasha <70953705+UnarineMashasha@users.noreply.github.com>
2025-05-17 04:52:28 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+const app = require("express")();
+const PORT = 3000;
+
+app.listen(PORT, () =>
+  console.log(`We have lift off on http://localhost:${PORT}`)
+);
+
+app.get("/tshirt", (req, res) => {
+  res.status(200).send({
+    tshirt: "Shirt",
+    size: "medium",
+  });
+});
+
+aa.post("/tshirt/:id", (req, res) => {
+  const { id } = req.params;
+  const { logo } = req.body;
+
+  if (!logo) {
+    res.status(418).send({
+      message: "We need a logo!",
+    });
+  }
+  res.send({
+    tshirt: `tshirt with your ${logo} and ID of ${id}`,
+  });
+});
